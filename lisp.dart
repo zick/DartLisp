ba12f04b2472579b462a778140335df5be164f97
zick <zickminoh@gmail.com>
2014-08-30 15:20:16 +0900

    Use class instead of map. Make symbols for keywords beforehand.
    This change makes DartLisp much faster (about 16 times)!

--- lisp.dart
+++ lisp.dart
@@ -5,61 +5,82 @@ import 'dart:convert';
 String kLPar = '(';
 String kRPar = ')';
 String kQuote = "'";
-var kNil = {
-  'tag': 'nil',
-  'data': 'nil'
-};
 
-safeCar(obj) => (obj['tag'] == 'cons') ? obj['car'] : kNil;
+class Nil {
+  const Nil();
+}
 
-safeCdr(obj) => (obj['tag'] == 'cons') ? obj['cdr'] : kNil;
+final kNil = const Nil();
 
-makeError(String str) => {
-  'tag': 'error',
-  'data': str
-};
+safeCar(obj) => obj is Cons ? obj.car : kNil;
+safeCdr(obj) => obj is Cons ? obj.cdr : kNil;
+
+class Error {
+  final data;
+  Error(this.data);
+}
+
+makeError(String str) => new Error(str);
 
 Map<String, Object> sym_table = {};
+
+class Sym {
+  final data;
+  Sym(this.data);
+}
+
 makeSym(String str) {
   if (str == 'nil') {
     return kNil;
   } else if (!sym_table.containsKey(str)) {
-    sym_table[str] = {
-      'tag': 'sym',
-      'data': str
-    };
+    sym_table[str] = new Sym(str);
   }
   return sym_table[str];
 }
 
-makeNum(num n) => {
-  'tag': 'num',
-  'data': n
-};
+final sym_t = makeSym('t');
+final sym_quote = makeSym('quote');
+final sym_if = makeSym('if');
+final sym_lambda = makeSym('lambda');
+final sym_defun = makeSym('defun');
+final sym_setq = makeSym('setq');
 
-makeCons(a, d) => {
-  'tag': 'cons',
-  'car': a,
-  'cdr': d
-};
+class Num {
+  final data;
+  Num(this.data);
+}
 
-makeSubr(fn) => {
-  'tag': 'subr',
-  'data': fn
-};
+makeNum(num n) => new Num(n);
 
-makeExpr(args, env) => {
-  'tag': 'expr',
-  'args': safeCar(args),
-  'body': safeCdr(args),
-  'env': env
-};
+class Cons {
+  var car;
+  var cdr;
+  Cons(this.car, this.cdr);
+}
+
+makeCons(a, d) => new Cons(a, d);
+
+class Subr {
+  final data;
+  Subr(this.data);
+}
+
+makeSubr(fn) => new Subr(fn);
+
+class Expr {
+  final args;
+  final body;
+  final env;
+  Expr(this.args, this.body, this.env);
+}
+
+makeExpr(args, env) => new Expr(safeCar(args), safeCdr(args), env);
 
 nreverse(lst) {
   var ret = kNil;
-  while (lst['tag'] == 'cons') {
-    var tmp = lst['cdr'];
-    lst['cdr'] = ret;
+  while (lst is Cons) {
+    var tmp = lst.cdr;
+    lst.cdr = ret;
     ret = lst;
     lst = tmp;
   }
@@ -68,10 +89,10 @@ nreverse(lst) {
 
 pairlis(lst1, lst2) {
   var ret = kNil;
-  while (lst1['tag'] == 'cons' && lst2['tag'] == 'cons') {
-    ret = makeCons(makeCons(lst1['car'], lst2['car']), ret);
-    lst1 = lst1['cdr'];
-    lst2 = lst2['cdr'];
+  while (lst1 is Cons && lst2 is Cons) {
+    ret = makeCons(makeCons(lst1.car, lst2.car), ret);
+    lst1 = lst1.cdr;
+    lst2 = lst2.cdr;
   }
   return nreverse(ret);
 }
@@ -106,12 +127,12 @@ read(String str) {
   if (str.length == 0) {
     return [makeError('empty input'), ''];
   } else if (str[0] == kRPar) {
-    return [makeError('invalid syntax: ' + str), ''];
+    return [makeError('invalid syntax: ${str}'), ''];
   } else if (str[0] == kLPar) {
     return readList(str.substring(1));
   } else if (str[0] == kQuote) {
     var tmp = read(str.substring(1));
-    return [makeCons(makeSym('quote'), makeCons(tmp[0], kNil)), tmp[1]];
+    return [makeCons(sym_quote, makeCons(tmp[0], kNil)), tmp[1]];
   }
   return readAtom(str);
 }
@@ -128,7 +149,7 @@ readList(String str) {
     var tmp = read(str);
     var elm = tmp[0];
     var next = tmp[1];
-    if (elm['tag'] == 'error') {
+    if (elm is Error) {
       return [elm, ''];
     }
     ret = makeCons(elm, ret);
@@ -138,15 +159,14 @@ readList(String str) {
 }
 
 printObj(obj) {
-  var tag = obj['tag'];
-  if (tag == 'num' || tag == 'sym' || tag == 'nil') {
-    return obj['data'].toString();
-  } else if (tag == 'error') {
-    return '<error: ' + obj['data'] + '>';
-  } else if (tag == 'cons') {
+  if (obj is Num || obj is Sym || obj is Nil) {
+    return obj.data.toString();
+  } else if (obj is Error) {
+    return '<error: ${obj.data}>';
+  } else if (obj is Cons) {
     return printList(obj);
-  } else if (tag == 'subr' || tag == 'expr') {
-    return tag;
+  } else if (obj is Subr || obj is Expr) {
+    return tag.runtimeType.toString();
   }
   return '<unknown>';
 }
@@ -154,31 +174,31 @@ printObj(obj) {
 printList(obj) {
   String ret = '';
   bool first = true;
-  while (obj['tag'] == 'cons') {
+  while (obj is Cons) {
     if (first) {
       first = false;
     } else {
       ret += ' ';
     }
-    ret += printObj(obj['car']);
-    obj = obj['cdr'];
+    ret += printObj(obj.car);
+    obj = obj.cdr;
   }
   if (obj == kNil) {
-    return '(' + ret + ')';
+    return '(${ret})';
   }
-  return '(' + ret + ' . ' + printObj(obj) + ')';
+  return '(${ret} . ${printObj(obj)})';
 }
 
 findVar(sym, env) {
-  while (env['tag'] == 'cons') {
-    var alist = env['car'];
-    while (alist['tag'] == 'cons') {
-      if (alist['car']['car'] == sym) {
-        return alist['car'];
+  while (env is Cons) {
+    var alist = env.car;
+    while (alist is Cons) {
+      if (alist.car.car == sym) {
+        return alist.car;
       }
-      alist = alist['cdr'];
+      alist = alist.cdr;
     }
-    env = env['cdr'];
+    env = env.cdr;
   }
   return kNil;
 }
@@ -186,45 +206,44 @@ findVar(sym, env) {
 var g_env = makeCons(kNil, kNil);
 
 addToEnv(sym, val, env) {
-  env['car'] = makeCons(makeCons(sym, val), env['car']);
+  env.car = makeCons(makeCons(sym, val), env.car);
 }
 
 eval(obj, env) {
-  var tag = obj['tag'];
-  if (tag == 'nil' || tag == 'num' || tag == 'error') {
+  if (obj is Nil || obj is Num || obj is Error) {
     return obj;
-  } else if (tag == 'sym') {
+  } else if (obj is Sym) {
     var bind = findVar(obj, env);
     if (bind == kNil) {
-      return makeError(obj['data'] + ' has no value');
+      return makeError('${obj.data} has no value');
     }
-    return bind['cdr'];
+    return bind.cdr;
   }
 
   var op = safeCar(obj);
   var args = safeCdr(obj);
-  if (op == makeSym('quote')) {
+  if (op == sym_quote) {
     return safeCar(args);
-  } else if (op == makeSym('if')) {
+  } else if (op == sym_if) {
     if (eval(safeCar(args), env) == kNil) {
       return eval(safeCar(safeCdr(safeCdr(args))), env);
     }
     return eval(safeCar(safeCdr(args)), env);
-  } else if (op == makeSym('lambda')) {
+  } else if (op == sym_lambda) {
     return makeExpr(args, env);
-  } else if (op == makeSym('defun')) {
+  } else if (op == sym_defun) {
     var expr = makeExpr(safeCdr(args), env);
     var sym = safeCar(args);
     addToEnv(sym, expr, g_env);
     return sym;
-  } else if (op == makeSym('setq')) {
+  } else if (op == sym_setq) {
     var val = eval(safeCar(safeCdr(args)), env);
     var sym = safeCar(args);
     var bind = findVar(sym, env);
     if (bind == kNil) {
       addToEnv(sym, val, g_env);
     } else {
-      bind['cdr'] = val;
+      bind.cdr = val;
     }
     return val;
   }
@@ -233,35 +252,35 @@ eval(obj, env) {
 
 evlis(lst, env) {
   var ret = kNil;
-  while (lst['tag'] == 'cons') {
-    var elm = eval(lst['car'], env);
-    if (elm['tag'] == 'error') {
+  while (lst is Cons) {
+    var elm = eval(lst.car, env);
+    if (elm is Error) {
       return elm;
     }
     ret = makeCons(elm, ret);
-    lst = lst['cdr'];
+    lst = lst.cdr;
   }
   return nreverse(ret);
 }
 
 progn(body, env) {
   var ret = kNil;
-  while (body['tag'] == 'cons') {
-    ret = eval(body['car'], env);
-    body = body['cdr'];
+  while (body is Cons) {
+    ret = eval(body.car, env);
+    body = body.cdr;
   }
   return ret;
 }
 
 apply(fn, args, env) {
-  if (fn['tag'] == 'error') {
+  if (fn is Error) {
     return fn;
-  } else if (args['tag'] == 'error') {
+  } else if (args is Error) {
     return args;
-  } else if (fn['tag'] == 'subr') {
-    return fn['data'](args);
-  } else if (fn['tag'] == 'expr') {
-    return progn(fn['body'], makeCons(pairlis(fn['args'], args), fn['env']));
+  } else if (fn is Subr) {
+    return fn.data(args);
+  } else if (fn is Expr) {
+    return progn(fn.body, makeCons(pairlis(fn.args, args), fn.env));
   }
   return makeError('noimpl');
 }
@@ -275,31 +294,31 @@ subrCons(args) => makeCons(safeCar(args), safeCar(safeCdr(args)));
 subrEq(args) {
   var x = safeCar(args);
   var y = safeCar(safeCdr(args));
-  if (x['tag'] == 'num' && y['tag'] == 'num') {
-    if (x['data'] == y['data']) {
-      return makeSym('t');
+  if (x is Num && y is Num) {
+    if (x.data == y.data) {
+      return sym_t;
     }
     return kNil;
   } else if (x == y) {
-    return makeSym('t');
+    return sym_t;
   }
   return kNil;
 }
 
-subrAtom(args) => (safeCar(args)['tag'] == 'cons') ? kNil : makeSym('t');
+subrAtom(args) => (safeCar(args) is Cons) ? kNil : sym_t;
 
-subrNumberp(args) => (safeCar(args)['tag'] == 'num') ? makeSym('t') : kNil;
+subrNumberp(args) => (safeCar(args) is Num) ? sym_t : kNil;
 
-subrSymbolp(args) => (safeCar(args)['tag'] == 'sym') ? makeSym('t') : kNil;
+subrSymbolp(args) => (safeCar(args) is Sym) ? sym_t : kNil;
 
 subrAddOrMul(fn, init_val) => (args) {
   var ret = init_val;
-  while (args['tag'] == 'cons') {
-    if (args['car']['tag'] != 'num') {
+  while (args is Cons) {
+    if (args.car is! Num) {
       return makeError('wrong type');
     }
-    ret = fn(ret, args['car']['data']);
-    args = args['cdr'];
+    ret = fn(ret, args.car.data);
+    args = args.cdr;
   }
   return makeNum(ret);
 };
@@ -309,10 +328,10 @@ var subrMul = subrAddOrMul((x, y) => x * y, 1);
 subrSubOrDivOrMod(fn) => (args) {
   var x = safeCar(args);
   var y = safeCar(safeCdr(args));
-  if (x['tag'] != 'num' || y['tag'] != 'num') {
+  if (x is! Num || y is! Num) {
     return makeError('wrong type');
   }
-  return makeNum(fn(x['data'], y['data']));
+  return makeNum(fn(x.data, y.data));
 };
 var subrSub = subrSubOrDivOrMod((x, y) => x - y);
 var subrDiv = subrSubOrDivOrMod((x, y) => x / y);
@@ -334,7 +353,7 @@ void main() {
   addToEnv(makeSym('-'), makeSubr(subrSub), g_env);
   addToEnv(makeSym('/'), makeSubr(subrDiv), g_env);
   addToEnv(makeSym('mod'), makeSubr(subrMod), g_env);
-  addToEnv(makeSym('t'), makeSym('t'), g_env);
+  addToEnv(sym_t, sym_t, g_env);
 
   stdout.write('> ');
   readLine().listen((String line) {
